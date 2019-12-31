c7f26590c503afcac38b3c1afa25f788673dca03
Olaf Lederer <olaf.lederer@gmail.com>
2019-12-31 10:21:48 +0100

    Added IP directory protection

---
+++ libs/ip_protect.php
@@ -0,0 +1,18 @@
+<?php
+include_once 'func.php';
+
+$str = 'order deny,allow
+deny from all
+
+# whitelist your own IP address
+allow from '.$_SERVER['REMOTE_ADDR'];
+
+$savePath = MYBACKUPDIR.'.htaccess';
+
+$handle = fopen($savePath, 'w+');
+if (fwrite($handle, $str) === FALSE) {
+    echo 'error';
+} else {
+    echo 'okay';
+}
+fclose($handle);
--- settings.php
+++ settings.php
@@ -1,5 +1,4 @@
 <?php
-<?php
 include_once 'libs/func.php';
 ?>
 <!DOCTYPE html>
@@ -29,7 +28,7 @@ include_once 'libs/func.php';
             <li class="active"><a href="index.php">Backup &amp; Restore</a></li>
             <li><a href="/">Home</a></li>
             <li><a href="../wp-admin/">WP Admin</a></li>
-            <li><a href="settings.php" target="_blank">Settings></li>
+             <li><a href="settings.php" target="_blank">Settings></a></li>
           </ul>
         </div><!--/.nav-collapse -->
       </div>
@@ -38,7 +37,9 @@ include_once 'libs/func.php';
       <div class="starter-template">
         <h1>Backup &amp; Restore for WordPress <small>beta</small></h1>
         <h2>Settings page</h2>
-        <p>Password protect the <strong><?php echo basename(__DIR__); ?><?php</strong> directory.<br>If you safe a new password, you will replace the old one (if exists).</p>
+        <h3>Protect the <em><?php echo basename(__DIR__); ?></em> directory.</h3>
+        <p>This function works only for Apache based web servers!</p>
+        <p>Add a login and password. If you safe a new password, the old one will be replaced (if exists).</p>
         <form class="form-inline">
           <div class="form-group">
             <label class="sr-only" for="loginname">Login name</label>
@@ -50,6 +51,8 @@ include_once 'libs/func.php';
           </div>
           <button type="submit" class="btn btn-primary">Save</button>
         </form>
+        <hr>
+        <p><a href="javascript:void(0);" id="addipadr"><strong>Allow the current IP address</strong></a> (<?php echo $_SERVER['REMOTE_ADDR']; ?>) to access the directory.</p>
 
 
 		<div id="msg" class="" role="alert"></div>
@@ -62,7 +65,21 @@ include_once 'libs/func.php';
 	<script>
 
 	jQuery(document).ready(function($) {
+        $('#addipadr').click(function(e) {
+			$('#msg').removeClass('alert alert-success').html('<img src="img/loadingAnimation.gif" alt="Please wait...">');
+			$.ajax({
+				url: "libs/ip_protect.php",
+                success: function (data) {
+					if (data == 'okay') {
+						$('#msg').addClass('alert alert-success').html('Only your IP address can access the directory from now on.');
 
+					} else {
+                        $('#msg').addClass('alert alert-success').html('Error, IP address protect is not installed.');
+					}
+                }
+			});
+			e.preventDefault();
+		});
 
 	});
 
